baf5dd88aa46c44712cc2a31863c2fa5d05ce039
José Luiz <luizriacho@gmail.com>
2026-03-16 13:29:36 -0300

    Atualização Março 16-03

--- UExcelService.pas
+++ UExcelService.pas
@@ -10,7 +10,6 @@ type
   private
     class procedure FormatarPlanilhaExcel(const Excel: Variant; const LinhaFinal, ColunaFinal: Integer);
   public
-    { Adicionado parâmetro Observacoes: TStrings }
     class procedure ExportarDataSet(DataSet: TDataSet; ValorTag: Integer; const TituloCabecalho: string; Observacoes: TStrings = nil);
   end;
 
@@ -39,7 +38,11 @@ begin
   LogoCliente := PathImages + 'logoCliente.png';
   LogoSic     := PathImages + 'logoSic.png';
 
+  // Garante que o RecordCount seja populado (necessário em alguns drivers Firebird)
+  DataSet.Last;
   TotalRegistros := DataSet.RecordCount;
+  DataSet.First;
+
   DataSet.DisableControls;
   try
     planilha := CreateOleObject('Excel.Application');
@@ -54,11 +57,14 @@ begin
       if DataSet.Fields[coluna].Tag = ValorTag then
         Inc(UltimaColunaPreenchida);
 
+    if UltimaColunaPreenchida = 0 then Exit;
+
     // 2. Ajuste de altura das linhas 1 e 2 para as logos
     Sheet.Rows.Item[1].RowHeight := 25;
     Sheet.Rows.Item[2].RowHeight := 25;
 
     // 3. Preparar Array de Dados
+    // Usamos varVariant para aceitar diversos tipos, mas trataremos o conteúdo abaixo
     Dados := VarArrayCreate([1, TotalRegistros + 1, 1, UltimaColunaPreenchida], varVariant);
 
     // Header
@@ -72,30 +78,46 @@ begin
 
     // Dados
     Linha := 2;
-    DataSet.First;
     while not DataSet.Eof do
     begin
       ColExcel := 1;
       for coluna := 0 to DataSet.FieldCount - 1 do
+      begin
         if DataSet.Fields[coluna].Tag = ValorTag then
         begin
           if DataSet.Fields[coluna].IsNull then
             Dados[Linha, ColExcel] := ''
-          else if DataSet.Fields[coluna] is TNumericField then
-            Dados[Linha, ColExcel] := DataSet.Fields[coluna].AsFloat
           else
-            Dados[Linha, ColExcel] := DataSet.Fields[coluna].Value;
+          begin
+            // Tratamento preventivo para tipos que o Excel/OLE costuma rejeitar
+            case DataSet.Fields[coluna].DataType of
+              ftLargeint, ftBCD, ftFMTBcd:
+                Dados[Linha, ColExcel] := DataSet.Fields[coluna].AsFloat;
+              ftBoolean:
+                Dados[Linha, ColExcel] := DataSet.Fields[coluna].AsBoolean;
+              ftDate, ftDateTime, ftTime:
+                Dados[Linha, ColExcel] := DataSet.Fields[coluna].AsDateTime;
+              else
+                // Para campos numéricos padrão ou texto
+                if DataSet.Fields[coluna] is TNumericField then
+                  Dados[Linha, ColExcel] := DataSet.Fields[coluna].AsFloat
+                else
+                  Dados[Linha, ColExcel] := DataSet.Fields[coluna].AsString;
+            end;
+          end;
           Inc(ColExcel);
         end;
+      end;
       Inc(Linha);
       DataSet.Next;
     end;
 
-    // 4. Despeja os dados
-    Sheet.Range[Sheet.Cells[3, 1], Sheet.Cells[3 + TotalRegistros, UltimaColunaPreenchida]].Value := Dados;
+    // 4. Despeja os dados - O erro ocorria aqui
+    vRange := Sheet.Range[Sheet.Cells[3, 1], Sheet.Cells[2 + Linha - 1, UltimaColunaPreenchida]];
+    vRange.Value := Dados;
 
-    // 5. Formata a Planilha (AutoFit e Estilo de Tabela)
-    FormatarPlanilhaExcel(planilha, 3 + TotalRegistros, UltimaColunaPreenchida);
+    // 5. Formata a Planilha
+    FormatarPlanilhaExcel(planilha, 2 + Linha - 1, UltimaColunaPreenchida);
 
     // 6. Configura o Título
     vRange := Sheet.Range[Sheet.Cells[1, 1], Sheet.Cells[2, UltimaColunaPreenchida]];
@@ -106,18 +128,16 @@ begin
     vRange.HorizontalAlignment := xlCenter;
     vRange.VerticalAlignment := xlCenter;
 
-    // 7. INSERÇÃO DAS OBSERVAÇÕES (RODAPÉ)
+    // 7. INSERÇÃO DAS OBSERVAÇÕES
     if Assigned(Observacoes) and (Observacoes.Count > 0) then
     begin
-      // Começa 2 linhas abaixo do fim da tabela
       LinhaRodape := 3 + TotalRegistros + 2;
-
       for i := 0 to Observacoes.Count - 1 do
       begin
         vRange := Sheet.Cells[LinhaRodape + i, 1];
         vRange.Value := Observacoes[i];
         vRange.Font.Italic := True;
-        vRange.Font.Color := $00555555; // Cinza escuro
+        vRange.Font.Color := $00555555;
       end;
     end;
 
@@ -149,12 +169,11 @@ begin
   if planilha.ListObjects.Count > 0 then
     planilha.ListObjects.Item(1).Delete;
 
-  Tabela := planilha.ListObjects.Add(1, RangeDados, False, 1, EmptyParam);
+  Tabela := planilha.ListObjects.Add(1, RangeDados, False, 1);
   Tabela.TableStyle := 'TableStyleLight2';
   Tabela.ShowAutoFilter := True;
 
   planilha.Columns.AutoFit;
-
   planilha.Cells[4, 1].Select;
   Excel.ActiveWindow.FreezePanes := True;
 end;
